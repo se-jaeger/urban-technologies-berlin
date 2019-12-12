Fix issue with negative gradients

notebooks/1.0-modelling.py
@@ -138,7 +138,11 @@ def water_flow_velocity(
     Returns:
         float: Matrix of the water flow velocities of shape ``n x 2`` in ``x`` and ``y`` direction.
     """
-    return kst * np.cbrt(R(water_in_liter, tile_square_meter) ** 2) * np.sqrt(gradients)
+    
+    gradient_direction = np.sign(gradients)
+    result_absolute = kst * np.cbrt(R(water_in_liter, tile_square_meter) ** 2) * np.sqrt(np.absolute(gradients))
+    
+    return gradient_direction * result_absolute
 
 
 # %%