d974167e4fb0f3a5b2136607cd05e7736848734d
Sebastian Jäger <message@sebastian-jaeger.me>
2019-12-15 17:52:19 +0100

    Add helper for calculating tile size

--- src/urban_technologies_berlin/utils.py
+++ src/urban_technologies_berlin/utils.py
@@ -63,6 +63,21 @@ def style_function_factory(column: str, color_a: str, color_b: str, value_range:
     return style_function
 
 
+def get_tile_size(data: GeoDataFrame) -> int:
+    """
+    Helper function that returns the size of a tile.
+
+    Args:
+        data (GeoDataFrame): Data that has the form of ground level information.
+
+    Returns:
+        int: Tile size in meter. Tiles are always squares.
+    """
+    geometry_coordinates = data[:1]["geometry"].values[0].exterior.xy[0]
+
+    return int(geometry_coordinates[1] - geometry_coordinates[0])
+
+
 def get_tile_square_meter(data: GeoDataFrame) -> int:
     """
     Helper funciton that computes the square meter of the given ``data``.
@@ -73,7 +88,4 @@ def get_tile_square_meter(data: GeoDataFrame) -> int:
     Returns:
         int: Square meter for one tile.
     """
-    geometry_coordinates = data[:1]["geometry"].values[0].exterior.xy[0]
-    tile_size = int(geometry_coordinates[1] - geometry_coordinates[0])
-
-    return tile_size ** 2  # this is okey because they are always squares
+    return get_tile_size(data) ** 2  # this is okey because they are always squares
